6e34bf7036b3fb4a31f8c17b0592ae24dff8b79b
ridhurshan <ridhurshan.ict@gmail.com>
2025-04-25 11:44:18 +0530

    after deployed commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/tamilloom/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
